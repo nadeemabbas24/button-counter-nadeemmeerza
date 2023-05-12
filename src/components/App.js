dad84d291f2ad8e01461f333ae0b17afcbffefd7
nadeem abbas <nadeemmeerza@gmail.com>
2023-05-12 07:55:52 +0530

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count, setCount] = useState(0);
+
+  function clickHandler(){
+    setCount(count+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={clickHandler}>click me</button>
     </div>
   )
 }
